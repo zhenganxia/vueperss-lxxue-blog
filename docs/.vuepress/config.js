6c2d6ebdd52bd72ad3b152aad4f77dacd829d7f7
liuxx <769461729@qq.com>
2023-08-03 21:26:03 +0800

    修改base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "峖小虾技术网站",
   description: "",
-  base: "/", // 设置站点根路径和github项目名称保持一致
+  base: "./", // 设置站点根路径和github项目名称保持一致
   plugins: [ // 配置插件
     'demo-container',
     'vuepress-plugin-cat',
